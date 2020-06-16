1073bbd092c79754302c4d1ad660e7cec4617545
LastGenius-edu <sultanov@ucu.edu.ua>
2020-06-16 14:11:44 +0300

    Start the actual text editor draw, initialize menu and main windows

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ def start():
     curses.cbreak()
     # Turning on keypad mode for easier custom keys support
     std_screen.keypad(True)
-
-    curses.curs_set(False)
+    # Turning the flickering pointer off
+    # curses.curs_set(False)
 
     if curses.has_colors():
         curses.start_color()
 
+    # Initialize a few main color pairs (foreground color, backround color)
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
@@ -40,6 +41,7 @@ def close(std_screen):
     curses.echo()
     curses.nocbreak()
     std_screen.keypad(False)
+    # curses.curs_set(True)
     curses.endwin()
 
 
@@ -47,11 +49,35 @@ def main(std_screen):
     """
     Main function of the module
     """
-    sleep(3)
-    std_screen.addstr("Text Editor", curses.A_REVERSE)
-    std_screen.chgat(-1, curses.A_REVERSE)
-    std_screen.refresh()
-    sleep(3)
+
+    # Add title and menu elements
+    std_screen.addstr("Text Editor", curses.A_REVERSE | curses.color_pair(2))
+    std_screen.chgat(-1, curses.A_REVERSE | curses.color_pair(2))
+    std_screen.addstr(curses.LINES - 1, 0, "Press 'q' to exit", curses.A_REVERSE)
+
+    # Create the editor window, draw the box around
+    text_editor = curses.newwin(curses.LINES - 2, curses.COLS, 1, 0)
+    text_editor.box()
+
+    # Create the subwindow for the actual text
+    text_box = text_editor.subwin(curses.LINES-4, curses.COLS-4, 2, 2)
+
+    # Refresh all the internal datastructures bottomup, update the screen
+    std_screen.noutrefresh()
+    text_editor.noutrefresh()
+    text_box.noutrefresh()
+    curses.doupdate()
+
+    while True:
+        char = text_editor.getch()
+
+        if char in (ord('q'), ord('Q')):
+            break
+
+        std_screen.noutrefresh()
+        text_editor.noutrefresh()
+        text_box.noutrefresh()
+        curses.doupdate()
 
 
 if __name__ == '__main__':
